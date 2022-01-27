e45f2fd6a8dcc05094d58d7b81f7890d3ae82703
Ricky Wang <ricky.wang@dayonedigital.com.au>
2022-01-27 22:35:27 +1100

    a

--- src/components/ContentArea/ContentArea.tsx
+++ src/components/ContentArea/ContentArea.tsx
@@ -8,41 +8,54 @@ interface IContentArea {
 
 const ContentArea = ({ text, style }: IContentArea) => {
   return (
-    <div>
+    <div
+      style={{
+        backgroundColor: Colours.DarkBlue,
+      }}
+    >
       <div
         style={{
-          backgroundColor: Colours.DarkBlue,
           padding: "50px",
-
+          display: "flex",
+          justifyContent: "center",
           color: "white",
           lineHeight: "1.5",
           fontSize: "20px",
           ...style,
         }}
       >
-        {text[0]}
+        <div
+          style={{
+            maxWidth: "1000px",
+          }}
+        >
+          {text[0]}
+        </div>
       </div>
-      <div
-        style={{
-          backgroundColor: Colours.DarkBlue,
-          display: "flex",
-          justifyContent: "space-around",
-          ...style,
-        }}
-      >
-        {text.slice(1, 3).map((x) => (
-          <div
-            style={{
-              padding: "30px",
-              maxWidth: "400px",
-              color: "white",
-              lineHeight: "1.5",
-              fontSize: "20px",
-            }}
-          >
-            {x}
-          </div>
-        ))}
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div
+          style={{
+            backgroundColor: Colours.DarkBlue,
+            display: "flex",
+            justifyContent: "space-between",
+            maxWidth: "1000px",
+            ...style,
+          }}
+        >
+          {text.slice(1, 3).map((x) => (
+            <div
+              style={{
+                padding: "30px",
+                maxWidth: "400px",
+                color: "white",
+                lineHeight: "1.5",
+                fontSize: "20px",
+              }}
+            >
+              {x}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
